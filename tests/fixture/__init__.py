e095d62f48febfacb3501c12e526e17e0cb53129
Luis Miranda <161006+luuuis@users.noreply.github.com>
2024-01-07 19:00:26 +0000

    ci: load fixture files as utf-8 to try to have tests pass in windows

--- tests/fixture/__init__.py
+++ tests/fixture/__init__.py
@@ -4,5 +4,5 @@ _FIXTURES_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def read_file(filename: str) -> str:
-    with open(os.path.join(_FIXTURES_DIR, filename)) as file:
+    with open(os.path.join(_FIXTURES_DIR, filename), encoding="utf-8") as file:
         return file.read()
